ac94cf8ba11e914ef81d95f533566a45035044ee
uttam.patra.kumar <uttampat@gmail.com>
2020-08-19 23:29:19 +0530

    Python Assignment Operators

---
+++ 09-Python-Operators/04-assignment-operators.py
@@ -0,0 +1,45 @@
+# Assignment operators
+# Assignment operators are used in Python to assign values to variables.
+
+# ******************************************************************************************
+
+# operator =
+x = 5
+print('operator x = 5 : ',x)
+
+# operator +=
+x = 5
+x += 5      # Equivalent to x = x + 5
+print('operator x += 5 : ',x)
+
+# operator -= 
+x = 5
+x -= 5     # Equivalent to x = x - 5
+print('operator x -= 5 : ',x)
+
+# operator *= 
+x = 5
+x *= 5     # Equivalent to x = x * 5
+print('operator x *= 5 : ',x)
+
+# operator /= 
+x = 5
+x /= 5     # Equivalent to x = x / 5
+print('operator x /= 5 : ',x)
+
+# operator //= 
+x = 5
+x //= 5     # Equivalent to x = x // 5
+print('operator x //= 5 : ',x)
+
+# operator %= 
+x = 5
+x %= 5     # Equivalent to x = x % 5
+print('operator x %= 5 : ',x)
+
+# operator **= 
+x = 5
+x **= 5     # Equivalent to x = x % 5
+print('operator x **= 5 : ',x)
+
+
