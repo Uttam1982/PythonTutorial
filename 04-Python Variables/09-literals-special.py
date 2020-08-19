c868088368cdbe21de04f50384efdbddbb2a42d4
uttam.patra.kumar <uttampat@gmail.com>
2020-08-19 16:11:24 +0530

    Special Literals

---
+++ 04-Python Variables/09-literals-special.py
@@ -0,0 +1,12 @@
+# Special literals
+
+# - Python contains one special literal i.e. None. 
+# - We use it to specify that the field has not been created.
+
+name = "Sam"
+age = None
+
+print(name)
+
+print(age)
+print(type(age)) # class 'NoneType'
