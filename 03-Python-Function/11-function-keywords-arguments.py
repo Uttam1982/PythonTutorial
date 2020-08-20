d657a34bcc9114641dc99a99089e1b47d082f74f
uttam.patra.kumar <uttampat@gmail.com>
2020-08-21 04:34:31 +0530

    Python **kwargs

---
+++ 03-Python-Function/11-function-keywords-arguments.py
@@ -0,0 +1,35 @@
+# Types of arguments
+# There may be several types of arguments which can be passed at the time of function call.
+
+# 1. Required arguments
+# 2. Default arguments
+# 3. Keyword arguments
+# 4. Variable-length arguments
+
+#********************************************************************************************
+# Keyword arguments
+#********************************************************************************************
+
+# Python provides the facility to pass the multiple keyword arguments which can be represented 
+# as **kwargs. 
+# 
+# It is similar as the *args but it stores the argument in the dictionary format.
+
+# This type of arguments is useful when we do not know the number of arguments in advance.
+
+def basket(**kwargs):
+  """
+  pass the multiple keyword arguments
+  """
+  print("type of passed argument is ",type(kwargs)) 
+  print(kwargs)
+
+basket(fruit='Banana')
+basket(fruit='Banana',vegetable='Carrot')
+
+
+
+
+
+
+
