39d1a1cd781f891bdc349e2da9b63c2f51d1e92d
uttam.patra.kumar <uttampat@gmail.com>
2020-08-26 14:41:23 +0530

    python dir() function example

---
+++ 06-Python-Modules/11-python-dir-function.py
@@ -0,0 +1,11 @@
+# The dir() built-in function
+# We can use the dir() function to find out names that are defined inside a module.
+
+a = 10
+b = 'python'
+import example
+import math
+print(dir())
+
+# All the names defined in our current namespace can be found out using 
+# the dir() function without any arguments.
