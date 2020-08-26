0524816f1a3a57fa9e3b2c1d78e4d4e1782d37a4
uttam.patra.kumar <uttampat@gmail.com>
2020-08-26 14:38:12 +0530

    python import renaming example

---
+++ 06-Python-Modules/05-python-import-renaming.py
@@ -0,0 +1,10 @@
+# import with renaming
+
+import math as m
+
+# We have renamed the math module as m. This can save us typing time in some cases.
+
+print("The value of pi = ",m.pi)
+#print("The value of pi = ",math.pi)
+
+
