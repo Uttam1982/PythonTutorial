7dced06cd00de3d21edcc6754229f4a91e35fa2c
uttam.patra.kumar <uttampat@gmail.com>
2020-08-26 14:36:54 +0530

    python import example

---
+++ 06-Python-Modules/03-python-import.py
@@ -0,0 +1,6 @@
+# We use the import keyword to import a module
+import example
+
+# Using the module name we can access the function using the dot . operator. 
+
+print("The sum of two numbers = ", example.add(2,4))
