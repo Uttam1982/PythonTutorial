dd2d10efd547d283137b8ac39c72ff924f65a260
uttam.patra.kumar <uttampat@gmail.com>
2020-08-26 14:31:59 +0530

    python abs() function

---
+++ 05-Build-in-function/01-python-abs.py
@@ -0,0 +1,24 @@
+#Python abs()
+#The abs() method returns the absolute value of the given number. 
+#If the number is a complex number, abs() returns its magnitude.
+
+#********************************************************************
+
+#Example 1: Get absolute value of a number
+i1 = -20
+print('Absolute value of ',i1,' is : ',abs(i1))
+
+f1 = - 30.34
+print('Absolute value of ',f1,' is : ',abs(f1))
+
+
+#********************************************************************
+
+#Example 2: Get magnitude of a complex number
+
+c1 = 3 - 4j
+print('Absolute value of ',c1,' is : ',abs(c1))
+
+
+#It can be computed using the distance formula: 
+# https://www.ixl.com/math/algebra-2/absolute-values-of-complex-numbers
