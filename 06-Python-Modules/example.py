04bc65410ca4d35360ebec821021ed4a248c454e
uttam.patra.kumar <uttampat@gmail.com>
2020-08-26 14:42:04 +0530

    example module file

---
+++ 06-Python-Modules/example.py
@@ -0,0 +1,13 @@
+# Python Module example
+"""
+  The function takes in two numbers and returns their sum.
+  """
+
+def add(a,b):
+  """
+  The function takes in two numbers and returns their sum.
+  """
+  return a + b
+
+
+# we have a function add() inside a module example.
