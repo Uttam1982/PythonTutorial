81f576cd30bb00728719fd15a511ea6b9a19edbc
uttam.patra.kumar <uttampat@gmail.com>
2020-08-27 22:23:16 +0530

    Tuples methods

---
+++ 08-Python-DataTypes/Tuples/09-methods-tuple.py
@@ -0,0 +1,16 @@
+# Tuple Methods
+# 1. Methods that add items or remove items are not available with tuple. 
+# 2. Only the following two methods are available.
+  # 1. count
+  # 2. index
+
+my_tuple = (10,20,30,20,40,50)
+
+# The count() method returns the number of times the specified element appears in the list.
+print("count the value of 20: ",my_tuple.count(20))
+
+#The index() method return the index of the first matching element 
+print("index of 20: ",my_tuple.index(20))
+
+
+
