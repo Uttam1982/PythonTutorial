e3e99159b0f5aa71c63ae62e09a45b8b7eca95c2
uttam.patra.kumar <uttampat@gmail.com>
2020-08-27 17:40:36 +0530

    How to delete or remove elements from a list

---
+++ 08-Python-DataTypes/Lists/10-delete-method.py
@@ -0,0 +1,31 @@
+# How to delete or remove elements from a list?
+# We can delete one or more items from a list using the keyword del. 
+# It can even delete the list entirely.
+
+my_list = [12,23,34,45,56,67,78,89,90]
+print(my_list)
+
+#delete 2nd element 
+del(my_list[1])
+print(my_list)
+
+# delete multiple items
+del(my_list[1 : 5])
+print(my_list)
+
+#index out of range 
+# del(my_list[10])
+# print(my_list)
+
+
+# Example 2: Emptying the List Using del
+del my_list[:]
+print(my_list)
+
+
+# delete entire list
+del my_list
+print(my_list)
+
+
+
