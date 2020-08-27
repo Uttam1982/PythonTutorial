61e735d109d5cbd3029c66438fcc89bed4e74ee5
uttam.patra.kumar <uttampat@gmail.com>
2020-08-27 22:23:54 +0530

    Iterating Through a Tuple

---
+++ 08-Python-DataTypes/Tuples/11-iterating-tuple.py
@@ -0,0 +1,9 @@
+# Iterating Through a Tuple
+# We can use a for loop to iterate through each item in a tuple.
+
+my_tuple = (12,23,34,45,56,67,78,89,90)
+
+for i in my_tuple:
+  print(i,end=' ')
+  
+print("\n")
