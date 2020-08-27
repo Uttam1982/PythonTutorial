cf6e16806d2202a1257140e869568c14a6eb0323
uttam.patra.kumar <uttampat@gmail.com>
2020-08-27 17:38:15 +0530

    append() method

---
+++ 08-Python-DataTypes/Lists/06-append-method-list.py
@@ -0,0 +1,20 @@
+# append() method
+# The append() method adds an item to the end of the list.
+# We can add one item to a list using the append() method
+
+# Appending and Extending lists in Python
+
+# Example 2: add one elemnt on the list
+my_list = [2,4,6,8]
+my_list.append(1)
+print(my_list)                        # [2, 4, 6, 8, 1]
+
+
+# Example 2: Adding List to a List
+my_list.append([1,3,5,7])
+print(my_list)                        # [2, 4, 6, 8, 1, [1, 3, 5, 7]]
+
+
+# If you need to add items of a list to another list 
+# (rather than the list itself), use the extend() method.
+
