3d350eae56d53b3f459b1bb49de2364c7f3dffe7
uttam.patra.kumar <uttampat@gmail.com>
2020-08-27 17:36:47 +0530

    Negative indexing

---
+++ 08-Python-DataTypes/Lists/03-negative-indexing-list.py
@@ -0,0 +1,23 @@
+# Negative indexing
+# 1. Python allows negative indexing for its sequences. 
+# The index of -1 refers to the last item, -2 to the second last item and so on.
+
+
+# Negative indexing in lists
+my_list = [12,23,34,45,56]
+
+#last element in the list
+print("last element : ",my_list[-1])
+
+#second last element in the list
+print("second last  element : ",my_list[-2])
+
+#first element in the list
+print("first element : ",my_list[-5])
+
+#Index error
+print("first element : ",my_list[-6])
+
+
+
+
