50277ce7b0e03c0bb297840b4acbdff092ac51b6
uttam.patra.kumar <uttampat@gmail.com>
2020-08-27 17:36:20 +0530

    How to create a list

---
+++ 08-Python-DataTypes/Lists/01-create-list.py
@@ -0,0 +1,21 @@
+# How to create a list?
+# In Python programming, a list is created by placing all the items (elements) 
+# inside square brackets [], separated by commas.
+
+# It can have any number of items and they may be of different types 
+# (integer, float, string etc.).
+
+#************************************************************************************
+# empty list
+my_list = []
+
+# list of integers
+my_list = [1,2,3,4,5]
+
+#list with mixed data types
+my_list = [12, 'Sara', 34.5]
+
+# list containing another list
+my_list =["sara",[10,20,30,40], ['hyd','chennai']]
+
+#************************************************************************************
