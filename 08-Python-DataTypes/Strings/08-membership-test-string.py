49cfc0c55f24b9910a760a6b57bceb36f5a1a8f3
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 02:24:22 +0530

    String Membership Test

---
+++ 08-Python-DataTypes/Strings/08-membership-test-string.py
@@ -0,0 +1,9 @@
+# String Membership Test
+# We can test if a substring exists within a string or not, using the keyword in.
+
+str1 = "python"
+
+print("'t' in 'python': ", 't' in str1) #True
+
+print("'th' not in 'python': ", 'th' not in str1) #Flase
+
