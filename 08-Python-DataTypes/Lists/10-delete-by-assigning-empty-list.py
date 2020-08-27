ca85d044b16db8d9b44cc56cdf5aea760473d2e9
uttam.patra.kumar <uttampat@gmail.com>
2020-08-27 17:45:14 +0530

    delete items in a list by assigning an empty list

---
+++ 08-Python-DataTypes/Lists/10-delete-by-assigning-empty-list.py
@@ -0,0 +1,15 @@
+# we can also delete items in a list by assigning an empty list to a slice of elements.
+
+
+
+my_list = [12,23,34,45,56,67,78,89,90]
+print(my_list)
+
+my_list[2:4] =[]
+# output [12, 23, 56, 67, 78, 89, 90]
+print(my_list)
+
+my_list[2:5] =[]
+# output [12, 23, 89, 90]
+print(my_list)
+
