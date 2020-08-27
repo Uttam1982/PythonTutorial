3f860dff7f5a9b35b3077b77d9bc19cd96036e72
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 02:22:17 +0530

    How to create a string in Python

---
+++ 08-Python-DataTypes/Strings/02-create-string.py
@@ -0,0 +1,29 @@
+# How to create a string in Python?
+#---------------------------------------------------------------------------------------------
+# Strings can be created by enclosing characters inside a single quote or double-quotes. 
+# Even triple quotes can be used in Python but generally used to represent multiline strings 
+# and docstrings.
+#**********************************************************************************************
+
+# create strings in python
+
+my_string = "Python"
+print(my_string)
+
+my_string = 'Python'
+print(my_string)
+
+my_string = '''Python'''
+print(my_string)
+
+# triple quotes string can extend multiple lines
+my_string = '''Welcome 
+to
+python programming !'''
+print(my_string)
+
+my_string = """Welcome 
+to
+python programming 
+world!"""
+print(my_string)
