f2409d39b911083553a0e9503c61feee58b95839
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 02:34:46 +0530

    String formatting with padding and alignment

---
+++ 08-Python-DataTypes/String-Format/06-string-formating-padding-alignment.py
@@ -0,0 +1,18 @@
+# Example : String formatting with padding and alignment
+
+# string padding with left alignment
+print("{:5}".format("sam"))
+
+# string padding with right alignment
+print("{:>5}".format("sam"))
+
+# string padding with center alignment
+print("{:^5}".format("sam"))
+
+# string padding with center alignment
+# and '*' padding character
+print("{:*^5}".format("sam"))
+
+#'0' padding character
+print("{:0^5}".format("sam"))
+
