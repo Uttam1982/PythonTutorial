b72227c32cd97aaaf796d1f604396a186cb309c5
uttam.patra.kumar <uttampat@gmail.com>
2020-08-27 22:23:33 +0530

    Tuple Membership Test

---
+++ 08-Python-DataTypes/Tuples/10-tuple-membership-test.py
@@ -0,0 +1,14 @@
+# 1. Tuple Membership Test
+# We can test if an item exists in a tuple or not, using the keyword in.
+
+my_tuple = (12,23,34,45,56,67,78,89,90)
+
+# In operation
+print("23 in my_tuple: ",23 in my_tuple) #True
+
+
+# not in operation
+print("23 not in my_tuple: ",23 not in my_tuple) #False
+
+# not in operation
+print("43 not in my_tuple: ",43 not in my_tuple) #True
