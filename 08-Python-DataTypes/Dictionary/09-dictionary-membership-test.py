8c2b2705cadf37f482e4ace61f6cc96b848c8a87
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 21:42:20 +0530

    Dictionary Membership Test

---
+++ 08-Python-DataTypes/Dictionary/09-dictionary-membership-test.py
@@ -0,0 +1,10 @@
+# Dictionary Membership Test
+# We can test if a key is in a dictionary or not using the keyword in. 
+# Notice that the membership test is only for the keys and not for the values.
+
+my_dict = {1:'Sara',2:'Alex',3:'Ben',4:'Dave'}
+
+#output True
+print("2 in my_dict: ",2 in my_dict)
+#output False
+print("4 not in my_dict: ",4 not in my_dict)
