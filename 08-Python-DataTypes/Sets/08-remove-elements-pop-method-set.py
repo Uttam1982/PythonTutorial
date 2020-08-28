a31b8d51c61a889515f65a2fc3a6b7badadeaf6a
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 21:33:00 +0530

    we can remove and return an item using the pop() method.

---
+++ 08-Python-DataTypes/Sets/08-remove-elements-pop-method-set.py
@@ -0,0 +1,19 @@
+# pop method()
+# 1. we can remove and return an item using the pop() method.
+# 2. Since set is an unordered data type, there is no way of 
+#    determining which item will be popped. It is completely 
+#    arbitrary.
+
+my_set = set('python')
+print(my_set)
+
+# pop an element
+# Output: random element
+print(my_set.pop())
+print(my_set)
+
+# pop an element
+# Output: random element
+print(my_set.pop())
+print(my_set)
+
