95af89a395519a6969b1eea7b8ffe64af101b42c
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 21:35:19 +0530

    Set Membership Test

---
+++ 08-Python-DataTypes/Sets/11-set-membership-test.py
@@ -0,0 +1,9 @@
+# Set Membership Test
+# We can test if an item exists in a set or not, using the in keyword.
+
+# initialize set
+my_set = set('python')
+
+print("t in 'python: ",'t' in my_set)
+print("t not in 'python: ",'t' not in my_set)
+
