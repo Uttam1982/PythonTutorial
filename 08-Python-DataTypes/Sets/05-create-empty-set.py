6a2b03a5720ef2bf3d472f8e3f19c8b484c1ae18
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 21:31:32 +0530

    Creating an empty set is a bit tricky.

---
+++ 08-Python-DataTypes/Sets/05-create-empty-set.py
@@ -0,0 +1,16 @@
+# Creating an empty set is a bit tricky.
+# Empty curly braces {} will make an empty dictionary in Python. 
+# To make a set without any elements, we use the set() function without any argument.
+
+# create empty set with {} will make an empty dictionary in Python
+my_set = {}
+
+# check data type of my_set
+print(type(my_set))
+
+# initialize a with set()
+my_set = set()
+
+# check data type of my_set
+print(type(my_set))
+
