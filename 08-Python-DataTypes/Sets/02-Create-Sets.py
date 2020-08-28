07f62e3e7d9eee5a560353178f1275667d8b3047
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 21:30:18 +0530

    Creating Python Sets

---
+++ 08-Python-DataTypes/Sets/02-Create-Sets.py
@@ -0,0 +1,20 @@
+# Creating Python Sets
+
+# 1. A set is created by placing all the items (elements) inside curly braces {}, 
+#    separated by comma, or by using the built-in set() function.
+
+# 2. It can have any number of items and they may be of different types 
+#    (integer, float, tuple, string etc.). 
+
+# 3. But a set cannot have mutable elements like lists, sets or dictionaries as its elements.
+
+#**********************************************************************************************
+
+# set of integers
+my_set = {41,22,13,54}
+print("set of integers: ",my_set) 
+
+# set of mixed data types
+my_set = {'Sara',23, 234.567,(1,2,3)}
+print("set of mixed data types: ",my_set) 
+
