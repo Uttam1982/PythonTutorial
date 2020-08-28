299d679a548103fbaf0c20803b552b1846257663
uttam.patra.kumar <uttampat@gmail.com>
2020-08-28 21:33:22 +0530

    remove all the items from a set using the clear() method.

---
+++ 08-Python-DataTypes/Sets/09-remove-elements-clear-method-set.py
@@ -0,0 +1,10 @@
+# We can also remove all the items from a set using the clear() method.
+
+# initialize my_set
+my_set = set('python')
+print(my_set)
+
+# clear my_set
+# Output: set()
+my_set.clear()
+print(my_set)
